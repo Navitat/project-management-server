7143fca7d7e044973f512e5b0d51b90cd848c6d7
navitat <anlon636@gmail.com>
2025-07-31 14:47:12 +0200

    feat: add DELETE route for project

--- routes/project.routes.js
+++ routes/project.routes.js
@@ -74,4 +74,26 @@ router.put("/projects/:projectId", (req, res, next) => {
     });
 });
 
+// DELETE /api/proejct/:projectId - Deletes specific project by id
+router.delete("/projects/:projectId", (req, res, next) => {
+  const { projectId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(projectId)) {
+    res.status(400).json({ message: "Specified id is not valid" });
+    return;
+  }
+
+  Project.findByIdAndDelete(projectId)
+    .then(() => {
+      res.json({
+        message: `Project with id: ${projectId} was removed successfully`,
+      });
+    })
+    .catch((error) => {
+      console.log("Error while deleting the project");
+      console.log(error);
+      res.status(500).json({ message: "Error while deleting the project" });
+    });
+});
+
 module.exports = router;
